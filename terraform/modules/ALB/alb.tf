a2fea241d9ccfd07753af680aa187220a27e1c87
Faith Odonghanro <toriboi.fo@gmail.com>
2023-01-01 12:15:36 +0100

    Uncomment listners and alb attachments

--- terraform/modules/ALB/alb.tf
+++ terraform/modules/ALB/alb.tf
@@ -34,33 +34,33 @@ resource "aws_lb_target_group" "nginx_tg" {
 }
 
 # HTTPS lisetner on External ALB
-# resource "aws_lb_listener" "nginx_listner_https" {
-#   load_balancer_arn = aws_lb.ext_alb.arn
-#   port              = 443
-#   protocol          = "HTTPS"
-#   certificate_arn   = aws_acm_certificate_validation.toritsejufo.certificate_arn
+resource "aws_lb_listener" "nginx_listner_https" {
+  load_balancer_arn = aws_lb.ext_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate_validation.toritsejufo.certificate_arn
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.nginx_tg.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx_tg.arn
+  }
+}
 
 # HTTP lisetner on External ALB to redirect traffic to https listener
-# resource "aws_lb_listener" "nginx_listner_http" {
-#   load_balancer_arn = aws_lb.ext_alb.arn
-#   port              = 80
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "nginx_listner_http" {
+  load_balancer_arn = aws_lb.ext_alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
-#   default_action {
-#     type = "redirect"
-#     redirect {
-#       port        = 443
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
-# }
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = 443
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
 
 # Internal ALB
 resource "aws_lb" "int_alb" {
@@ -117,31 +117,31 @@ resource "aws_lb_target_group" "tooling_tg" {
 
 # For this aspect a single listener was created for the wordpress which is default,
 # A rule was created to route traffic to tooling when the host header changes
-# resource "aws_lb_listener" "web_listener" {
-#   load_balancer_arn = aws_lb.int_alb.arn
-#   port              = 443
-#   protocol          = "HTTPS"
-#   certificate_arn   = aws_acm_certificate_validation.toritsejufo.certificate_arn
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.int_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate_validation.toritsejufo.certificate_arn
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.wordpress_tg.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wordpress_tg.arn
+  }
+}
 
 # Listener rule for tooling target
-# resource "aws_lb_listener_rule" "tooling_listener" {
-#   listener_arn = aws_lb_listener.web_listener.arn
-#   priority     = 99
+resource "aws_lb_listener_rule" "tooling_listener" {
+  listener_arn = aws_lb_listener.web_listener.arn
+  priority     = 99
 
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.tooling_tg.arn
-#   }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tooling_tg.arn
+  }
 
-#   condition {
-#     host_header {
-#       values = ["tooling.toritsejufo.ml"]
-#     }
-#   }
-# }
+  condition {
+    host_header {
+      values = ["tooling.toritsejufo.ml"]
+    }
+  }
+}
